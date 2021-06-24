b62baec5f55e74066f5c24bfb94c2267bc9f1bf8
Tomas <tomascanepa1397@gmail.com>
2021-06-24 18:19:06 -0300

    added GUI and modified game logic

--- scripts/main.js
+++ scripts/main.js
@@ -14,53 +14,96 @@ function playRound(playerSelection, computerSelection){
     if(playerSelection === computerSelection){
         return "It's a tie";
     } else if(playerSelection === "rock" && computerSelection === "paper"){
+        computerScore++;
         return "You lose! Paper beats rock";
     } else if(playerSelection === "rock" && computerSelection === "scissors"){
+        playerScore++;
         return "You win! Rock beats scissors"; 
     } else if(playerSelection === "paper" && computerSelection === "rock"){
+        playerScore++;
         return "You win! Paper beats rock";
     } else if(playerSelection === "paper" && computerSelection === "scissors"){
+        computerScore++;
         return "You lose! Scissors beat paper";
     } else if(playerSelection === "scissors" && computerSelection === "rock"){
+        computerScore++;
         return "You lose! Rock beats scissors";
     } else if(playerSelection === "scissors" && computerSelection === "paper"){
+        playerScore++;
         return "You win! Scissors beat paper";
     }
 }
 
 function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let playerSelection = ""
+    
     let computerSelection = "";
     let result;
-    for(let i = 0; i<5; i++){
-        // validating the user's entry so that the only valid inputs are rock, paper and scissors
-        while (playerSelection.toLowerCase() != "rock" && playerSelection.toLowerCase() != "paper" && playerSelection.toLowerCase() != "scissors"){
-            playerSelection = prompt("Choose: rock, paper or scissors?");
-        }
-        playerSelection = playerSelection.toLowerCase();
-        computerSelection = computerPlay();
-        result = playRound(playerSelection, computerSelection)
-        console.log(result);
-        playerSelection = "";
-        // update the scores
-        if(result.includes("win")){
-            playerScore++;
-        } else if(result.includes("lose")){
-            computerScore++;
-        }
-    }
-    if(playerScore > computerScore){
-        console.log("You win!");
-    } else if(playerScore < computerScore){
-        console.log("You lose");
-    } else{
-        console.log("It's a tie");
+    
+    computerSelection = computerPlay();
+    result = playRound(playerSelection, computerSelection)
+    console.log(result);
+    playerSelection = "";
+    // update the scores
+    if(result.includes("win")){
+        playerScore++;
+    } else if(result.includes("lose")){
+        computerScore++;
     }
 }
 
-game();
+function updateScore(){
+    scoreP.textContent = `You: ${playerScore} / Computer: ${computerScore}`;
+}
+
+let gameStarted = false;
+let playerScore = 0;
+let computerScore = 0;
+
+const container = document.querySelector(".container");
+const startButton = document.querySelector("#start");
+const scoreP = document.querySelector("#score");
+const options = document.querySelectorAll(".option");
+const message = document.querySelector("#message");
+
+startButton.addEventListener("click", () => {
+    gameStarted = true;
+    playerScore = 0;
+    computerScore = 0;
+    startButton.remove();
+    updateScore();
+});
+
+options.forEach((option) => {
+    option.addEventListener("click", () => {
+        if(computerScore < 5 && playerScore < 5 && gameStarted){
+            let computer = computerPlay();
+            let result = playRound(computer, option.id);
+            updateScore();
+            message.textContent = result;
+            if(computerScore === 5 || playerScore === 5){
+                gameStarted = false;
+                message.textContent = "";
+                let playAgain = "";
+                if(computerScore === 5){
+                    playAgain = "You lost, reload the page to play again";
+                } else{
+                    playAgain = "You won! Reload the page to play again";
+                }
+                scoreP.textContent = playAgain;
+            }
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
